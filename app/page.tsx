5cdb1d4305ece59e8a27f9cab8dafb162b567ba0
miguepoloc <miguepoloc@gmail.com>
2023-10-02 23:50:12 -0500

    :sparkles: feat: add page nodes

--- app/page.tsx
+++ app/page.tsx
@@ -11,14 +11,14 @@ export default function Home() {
 	return (
 		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
 			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>Make&nbsp;</h1>
-				<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
+				<h1 className={title()}>Sistemas&nbsp;</h1>
+				<h1 className={title({ color: "violet" })}>Inteligentes&nbsp;</h1>
 				<br />
 				<h1 className={title()}>
-					websites regardless of your design experience.
+					Datos de nodos sensores de la red de monitoreo de banano
 				</h1>
 				<h2 className={subtitle({ class: "mt-4" })}>
-					Beautiful, fast and modern React UI library.
+					GIDEAM
 				</h2>
 			</div>
 
